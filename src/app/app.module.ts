b358c2550a81b4ee8b911463be8e8604750865f4
Pavel Kim <peacelovecookiesstudy@gmail.com>
2021-03-31 17:29:37 +0500

    create main page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeadingComponent } from './heading/heading.component';
 import { CardsComponent } from './cards/cards.component';
 import { NavigatorComponent } from './navigator/navigator.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadingComponent,
     CardsComponent,
-    NavigatorComponent
+    NavigatorComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
